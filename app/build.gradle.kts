d7f311c5f910ce67ba2e6d68f5ed8bcf189bd737
zokirjonkarimov <zokirjonkarimov23@gmail.com>
2025-02-19 10:53:05 +0500

    fdfsfdsf

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "uz.isds.meterdetection"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
